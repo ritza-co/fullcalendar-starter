603e92de1f279722999956427da80a8289dbf73f
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2023-04-03 15:54:14 +0200

    Update the uodate function

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'tr3er00ts',
-  database: 'fullcalendar',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 db.connect((err) => {
