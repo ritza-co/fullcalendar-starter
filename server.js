984defb549473299f704e99ee3f47c5e171b5da6
Tatenda <31291528+rideam@users.noreply.github.com>
2023-04-11 02:26:27 +0200

    load environment vars and use public static folder

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const cors = require('cors');
+const path = require("path");
+require('dotenv').config()
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -63,6 +67,7 @@ app.delete('/events/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
